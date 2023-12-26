945c4a29a06786ffe9c4dc85280ebb63d27a450f
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-12-26 23:38:09 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,11 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "config.env" });
+
+const port = process.env.RUNNING_PORT || 5006;
+// const port = process.env.RUNNING_PORT;
+
+app.listen(port, () => {
+  console.log(`Listening on port http://localhost:${port}/api/v1`);
+});
